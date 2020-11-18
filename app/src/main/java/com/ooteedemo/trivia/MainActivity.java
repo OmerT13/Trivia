b83ad2227ba923f95f2fc48519e006b5c5544603
OT13 <omertahir13@gmail.com>
2020-11-18 08:38:09 +0800

    Linked our main activity resources to the java code. Populated the question textview and the previous/next buttons

--- app/src/main/java/com/ooteedemo/trivia/MainActivity.java
+++ app/src/main/java/com/ooteedemo/trivia/MainActivity.java
@@ -3,6 +3,10 @@ package com.ooteedemo.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.ooteedemo.trivia.data.AnswerListAsyncResponse;
 import com.ooteedemo.trivia.data.QuestionBank;
@@ -11,18 +15,68 @@ import com.ooteedemo.trivia.model.Question;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView questionTextView;
+    private TextView questionCounterTextView;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton prevButton;
+    private ImageButton nextButton;
+
+    private int currentQuestionIndex=0;
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Question> questionList =  new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+        prevButton = findViewById(R.id.prev_button);
+        nextButton = findViewById(R.id.next_button);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        questionCounterTextView = findViewById(R.id.counter_textView);
+        questionTextView = findViewById(R.id.question_textView);
+
+        prevButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+
+            questionList =  new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
+                questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
                 Log.d("MYMAIN", "onCreate: "+questionArrayList);
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.prev_button:
+                if (currentQuestionIndex>0){
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionList.size();
+                updateQuestion();
+            }
+                break;
+            case R.id.next_button:
+//                currentQuestionIndex++;
+                currentQuestionIndex = (currentQuestionIndex +1) % questionList.size();
+                updateQuestion();
+                break;
+            case R.id.true_button:
+                break;
+            case R.id.false_button:
+                break;
+        }
+
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        questionTextView.setText(question);
     }
 }
